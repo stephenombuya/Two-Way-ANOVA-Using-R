36bf61934a0fcc62adcc26acf5ed71e23681584c
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2026-04-03 13:09:35 +0300

    Add load_data function to load CSV files

--- R/01_load_data.R
+++ R/01_load_data.R
@@ -1,1 +1,11 @@
+library(readr)
+source("R/utils.R")
 
+load_data <- function(file_name) {
+  path <- paste0(DATA_RAW_PATH, file_name)
+  log_message(paste("Loading data from:", path))
+
+  df <- read_csv(path, show_col_types = FALSE)
+
+  return(df)
+}
