ca50f47c9c5e6dd3b92d03a30f3c72713e99c2e3
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2026-04-03 13:11:30 +0300

    Add plotting functions for distribution and interaction
    Added functions to plot distribution and interaction plots using ggplot2. Included saving functionality for generated plots.

--- R/03_exploratory_analysis.R
+++ R/03_exploratory_analysis.R
@@ -1,1 +1,27 @@
+library(ggplot2)
+source("R/utils.R")
 
+plot_distribution <- function(df) {
+
+  p <- ggplot(df, aes_string(x = DEPENDENT_VAR)) +
+    geom_histogram(bins = 30) +
+    theme_minimal() +
+    ggtitle("Distribution of Dependent Variable")
+
+  ensure_dir(OUTPUT_PLOTS_PATH)
+  ggsave(paste0(OUTPUT_PLOTS_PATH, "distribution.png"), plot = p)
+
+  log_message("Saved distribution plot")
+}
+
+plot_interaction <- function(df) {
+
+  p <- ggplot(df, aes_string(x = FACTOR_1, y = DEPENDENT_VAR, color = FACTOR_2)) +
+    stat_summary(fun = mean, geom = "line", aes(group = df[[FACTOR_2]])) +
+    theme_minimal() +
+    ggtitle("Interaction Plot")
+
+  ggsave(paste0(OUTPUT_PLOTS_PATH, "interaction_plot.png"), plot = p)
+
+  log_message("Saved interaction plot")
+}
